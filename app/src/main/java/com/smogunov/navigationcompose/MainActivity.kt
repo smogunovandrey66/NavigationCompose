402dc055b7f44c36f397999009693cb1f26c1a3d
SmogunovAV <smogunov@morion.ru>
2023-03-27 13:00:40 +0500

    Basic app

--- app/src/main/java/com/smogunov/navigationcompose/MainActivity.kt
+++ app/src/main/java/com/smogunov/navigationcompose/MainActivity.kt
@@ -1,30 +1,64 @@
 package com.smogunov.navigationcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SnapshotMutationPolicy
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.structuralEqualityPolicy
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.smogunov.navigationcompose.model.Screen
+import com.smogunov.navigationcompose.ui.components.CheckBoxText
+import com.smogunov.navigationcompose.ui.components.ComboBox
 import com.smogunov.navigationcompose.ui.theme.NavigationComposeTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+//            NavigationComposeTheme {
+            // A surface container using the 'background' color from the theme
+            NavApp()
+//            }
         }
     }
 }
@@ -37,10 +71,92 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NavigationComposeTheme {
-        Greeting("Android")
+        Greeting("")
     }
 }
+
+@Preview
+@Composable
+fun NavApp() {
+    Column(Modifier.fillMaxSize()) {
+        var needPopUpTo by remember {
+            mutableStateOf(false)
+        }
+        var inclusive by remember {
+            mutableStateOf(false)
+        }
+        var saveState by remember {
+            mutableStateOf(false)
+        }
+        var restoreState by remember {
+            mutableStateOf(false)
+        }
+        var launchSingleTop by remember {
+            mutableStateOf(false)
+        }
+        val screens = Screen.values().map {
+            it.name
+        }
+        val navController = rememberNavController()
+        Box(
+            Modifier
+                .border(2.dp, Color.Red, RoundedCornerShape(10.dp))
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Column {
+                CheckBoxText(
+                    needPopUpTo,
+                    stringResource(id = R.string.need_popup),
+                    { needPopUpTo = it })
+
+                if (needPopUpTo) {
+                    Row {
+                        CheckBoxText(inclusive, stringResource(id = R.string.inclusive), { inclusive = it })
+                        CheckBoxText(
+                            saveState,
+                            stringResource(id = R.string.save_state),
+                            { saveState = it })
+                    }
+                    Row {
+                        CheckBoxText(
+                            restoreState,
+                            stringResource(id = R.string.restore_state),
+                            { restoreState = it })
+                        CheckBoxText(
+                            launchSingleTop,
+                            stringResource(id = R.string.launch_single_top),
+                            { launchSingleTop = it })
+                    }
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(stringResource(R.string.popup_to), Modifier.padding(5.dp))
+                        ComboBox(items = screens)
+                    }
+                }
+            }
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+            Text(stringResource(R.string.navigate_to), Modifier.padding(5.dp))
+            ComboBox(items = screens)
+        }
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+            Button(onClick = {}) {
+                Text(text = stringResource(id = R.string.navigate_to))
+            }
+        }
+        NavHost(navController = navController, startDestination = screens.first()) {
+            screens.forEach { screen ->
+                composable(screen) {
+                    Text(screen)
+                }
+            }
+        }
+    }
+}
+
+
+
